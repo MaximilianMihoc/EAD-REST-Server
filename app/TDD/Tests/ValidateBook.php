5e4a10bd17422e4220dd1bee28eb2232f25d6f8e
adamm <adam.miedziejewski@mydit.ie>
2016-04-27 17:56:00 +0100

    TDD - books and users

--- app/TDD/Tests/ValidateBook.php
+++ app/TDD/Tests/ValidateBook.php
@@ -16,8 +16,55 @@ class ValidateBook extends UnitTestCase
 		$this->assertTrue($this->validation->isBookValid($arr));
 		
 		//Invalid tests
+		$arr = array (	"title" => "", "authorID" => "1", "ISBN" => "978-1-4493-8841-6", "pages" => "710",
+						"rating" => "3.50", "publisher" => "O'Reilly Media, Inc.", "publicationDate" => "2012-04-20" );
+		$this->assertFalse($this->validation->isBookValid($arr));
+		$arr = array (	"title" => "Android Cookbook", "authorID" => "", "ISBN" => "978-1-4493-8841-6", "pages" => "710",
+						"rating" => "3.50", "publisher" => "O'Reilly Media, Inc.", "publicationDate" => "2012-04-20" );
+		$this->assertFalse($this->validation->isBookValid($arr));
+		$arr = array (	"title" => "Android Cookbook", "authorID" => "1", "ISBN" => "", "pages" => "710",
+						"rating" => "3.50", "publisher" => "O'Reilly Media, Inc.", "publicationDate" => "2012-04-20" );
+		$this->assertFalse($this->validation->isBookValid($arr));
+		$arr = array (	"title" => "Android Cookbook", "authorID" => "1", "ISBN" => "978-1-4493-8841-6", "pages" => "",
+						"rating" => "3.50", "publisher" => "O'Reilly Media, Inc.", "publicationDate" => "2012-04-20" );
+		$this->assertFalse($this->validation->isBookValid($arr));
+		$arr = array (	"title" => "Android Cookbook", "authorID" => "1", "ISBN" => "978-1-4493-8841-6", "pages" => "710",
+						"rating" => "", "publisher" => "O'Reilly Media, Inc.", "publicationDate" => "2012-04-20" );
+		$this->assertFalse($this->validation->isBookValid($arr));
+		$arr = array (	"title" => "Android Cookbook", "authorID" => "1", "ISBN" => "978-1-4493-8841-6", "pages" => "710",
+						"rating" => "3.50", "publisher" => "", "publicationDate" => "2012-04-20" );
+		$this->assertFalse($this->validation->isBookValid($arr));
+		$arr = array (	"title" => "Android Cookbook", "authorID" => "1", "ISBN" => "978-1-4493-8841-6", "pages" => "710",
+						"rating" => "3.50", "publisher" => "O'Reilly Media, Inc.", "publicationDate" => "" );
+		$this->assertFalse($this->validation->isBookValid($arr));
+		
 		$arr = array (	"titles" => "Android Cookbook", "authorID" => "1", "ISBN" => "978-1-4493-8841-6", "pages" => "710",
 						"rating" => "3.50", "publisher" => "O'Reilly Media, Inc.", "publicationDate" => "2012-04-20" );
+		$this->assertFalse($this->validation->isBookValid($arr));
+		$arr = array (	"title" => "Android Cookbook", "authorIDs" => "1", "ISBN" => "978-1-4493-8841-6", "pages" => "710",
+						"rating" => "3.50", "publisher" => "O'Reilly Media, Inc.", "publicationDate" => "2012-04-20" );
+		$this->assertFalse($this->validation->isBookValid($arr));
+		$arr = array (	"title" => "Android Cookbook", "authorID" => "1", "ISBNs" => "978-1-4493-8841-6", "pages" => "710",
+						"rating" => "3.50", "publisher" => "O'Reilly Media, Inc.", "publicationDate" => "2012-04-20" );
+		$this->assertFalse($this->validation->isBookValid($arr));
+		$arr = array (	"title" => "Android Cookbook", "authorID" => "1", "ISBN" => "978-1-4493-8841-6", "pagess" => "710",
+						"rating" => "3.50", "publisher" => "O'Reilly Media, Inc.", "publicationDate" => "2012-04-20" );
+		$this->assertFalse($this->validation->isBookValid($arr));
+		$arr = array (	"title" => "Android Cookbook", "authorID" => "1", "ISBN" => "978-1-4493-8841-6", "pages" => "710",
+						"ratings" => "3.50", "publisher" => "O'Reilly Media, Inc.", "publicationDate" => "2012-04-20" );
+		$this->assertFalse($this->validation->isBookValid($arr));
+		$arr = array (	"title" => "Android Cookbook", "authorID" => "1", "ISBN" => "978-1-4493-8841-6", "pages" => "710",
+						"rating" => "3.50", "publishers" => "O'Reilly Media, Inc.", "publicationDate" => "2012-04-20" );
+		$this->assertFalse($this->validation->isBookValid($arr));
+		$arr = array (	"title" => "Android Cookbook", "authorID" => "1", "ISBN" => "978-1-4493-8841-6", "pages" => "710",
+						"rating" => "3.50", "publisher" => "O'Reilly Media, Inc.", "publicationDates" => "2012-04-20" );
+		$this->assertFalse($this->validation->isBookValid($arr));
+		
+		$arr = array (	"" );
+		$this->assertFalse($this->validation->isBookValid($arr));
+		
+		$arr = '{ "titles":"Android Cookbook", "authorID":"1", "ISBN":"978-1-4493-8841-6", "pages":"710",
+						"rating":"3.50", "publisher":"O\'Reilly Media, Inc.", "publicationDate":"2012-04-20" }';
 		$this->assertFalse($this->validation->isBookValid($arr));
 	}
 
--- app/TDD/Tests/ValidateUser.php
+++ app/TDD/Tests/ValidateUser.php
@@ -10,12 +10,40 @@ class ValidateUser extends UnitTestCase
 	public function testIsUserValid()
 	{
 		//Valid tests 
-		$arr = array ( "id" => "15","name" => "Adam","surname" => "Miedodsn","email" => "adamdo@jaja.clo","password" => "123456" );
+		$arr = array ( "name" => "Adam","surname" => "Miedodsn","email" => "adamdo@jaja.clo","password" => "123456" );
 		$this->assertTrue($this->validation->isUserValid($arr));
 		
 		//Invalid tests
-		$arr = array (	"names" => "Adam","surname" => "Miedodsn","email" => "adamdo@jaja.clo","password" => "123456" );
+		$arr = array ( "name" => "","surname" => "Miedodsn","email" => "adamdo@jaja.clo","password" => "123456" );
 		$this->assertFalse($this->validation->isUserValid($arr));
+		$arr = array ( "name" => "Adam","surname" => "","email" => "adamdo@jaja.clo","password" => "123456" );
+		$this->assertFalse($this->validation->isUserValid($arr));
+		$arr = array ( "name" => "Adam","surname" => "Miedodsn","email" => "","password" => "123456" );
+		$this->assertFalse($this->validation->isUserValid($arr));
+		$arr = array ( "name" => "Adam","surname" => "Miedodsn","email" => "adamdo@jaja.clo","password" => "" );
+		$this->assertFalse($this->validation->isUserValid($arr));
+		
+		$arr = array ( "names" => "Adam","surname" => "Miedodsn","email" => "adamdo@jaja.clo","password" => "123456" );
+		$this->assertFalse($this->validation->isUserValid($arr));
+		$arr = array ( "name" => "Adam","surnames" => "Miedodsn","email" => "adamdo@jaja.clo","password" => "123456" );
+		$this->assertFalse($this->validation->isUserValid($arr));
+		$arr = array ( "name" => "Adam","surname" => "Miedodsn","emails" => "adamdo@jaja.clo","password" => "123456" );
+		$this->assertFalse($this->validation->isUserValid($arr));
+		$arr = array ( "name" => "Adam","surname" => "Miedodsn","email" => "adamdo@jaja.clo","passwords" => "123456" );
+		$this->assertFalse($this->validation->isUserValid($arr));
+		
+		$arr = array ( "name" => "AdamAdamAdamAdamAdamAdamAdam","surname" => "Miedodsn","email" => "adamdo@jaja.clo","password" => "123456" );
+		$this->assertFalse($this->validation->isUserValid($arr));
+		
+		$arr = array ( "name" => "Adam","surname" => "MiedodsnMiedodsnMiedodsnaa","email" => "adamdo@jaja.clo","password" => "123456" );
+		$this->assertFalse($this->validation->isUserValid($arr));
+		
+		$arr = array ( "" );
+		$this->assertFalse($this->validation->isUserValid($arr));
+		
+		$arr = '{ "name":"Adam", "surname":"Miedodsn", "email":"adamdo@jaja.clo", "password":"123456" }';
+		$this->assertFalse($this->validation->isUserValid($arr));
+		
 	}
 
 	public function tearDown () 
